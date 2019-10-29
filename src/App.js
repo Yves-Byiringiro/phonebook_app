d11c3d89d36ab061c79448961842cd817d866894
Yves <byives21@gmail.com>
2019-10-29 15:07:57 -0400

    adding not found page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import {Home} from './components/Home/Home';
 import {ContactDetails} from './components/ContactDetails/ContactDetails';
 import {AddNewContact} from './components/AddNewContact/AddNewContact';
 import {EditContact} from './components/EditContact/EditContact';
+import NotFound from './components/pages/NotFound';
+
 
 
 
@@ -21,16 +23,12 @@ class App extends Component {
          <div>
           <Header/>
           <div className="container">
-            <Switch>
-              <Route exact path="/" component ={Home}/>
-              <Route exact path="/ContactDetails/:id" component={ContactDetails}/>
+          <Switch>
               <Route exact path="/AddNewContact" component={AddNewContact}/>
+              <Route exact path="/" component ={Home}/>
               <Route exact path="/EditContact/:id" component={EditContact}/>
-
-
-
-
-             
+              <Route exact path="/ContactDetails/:id" component={ContactDetails}/>
+              <Route component={NotFound}/>
             </Switch>
           </div>
           <Footer/>
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+//////////  Not found component section  ////////
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>404 Page Not Found</h1>
+            <p>Sorry!!, that page does not exist</p>
+        </div>
+    );
+}
+
+export default NotFound;
+ 
