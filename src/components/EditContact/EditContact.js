9f95d9829445c543813d4e27a84eea9eee5d2d2c
Yves <byiringiroyves127@gmail.com>
2020-01-06 05:29:59 +0200

    end of updating

--- src/components/EditContact/EditContact.js
+++ src/components/EditContact/EditContact.js
@@ -23,14 +23,20 @@ export class EditContact extends React.Component{
             const contact = res.data;
             const fullname = contact.name
             const hello = fullname.split(' ')
-            const firtname1 = hello[0];
-            const surname1 = hello[1];
-    
+            const firtname1 = hello[1];
+            const surname1 = hello[2];
+            console.log('phone number is here: '+contact.phone_number)
+            console.log('fullname is here:'+  contact.name)
+            console.log(hello)
+            console.log('firstname here: '+firtname1)
+            console.log('surname here: '+ surname1)
+            
             this.setState({
-                name: firtname1+" "+surname1,
-                firstName: firtname1,
-                surName: surname1,
-                phone_number: contact.phone_number
+                // name: firtname1+" "+surname1,
+                firstname: firtname1,
+                surname: surname1,
+                phone_number: contact.phone_number,
+                
     
             });
     
@@ -65,43 +71,43 @@ export class EditContact extends React.Component{
     
         return(
             
-                <div className="container">
-                    <div className="div1">
-                        <div className="user-image">
-                            <i className="fas fa-camera fa-10x"></i>
+        <div className="container">
+            <div className="div1">
+                <div className="user-image">
+                    <i className="fas fa-camera fa-10x"></i>
+                </div>
+                <div className="user-username">
+                    <form onSubmit={this.handleSubmit}>
+
+                        <label htmlFor="name">Firstname</label><br/>
+                        <div>
+                            <div style={{ color:'red'}}></div>
+                            <input type="text" name ="name" value={this.state.firstname} onChange={e => this.setState({ firstname: e.target.value })} placeholder="Firstname" /><br/>
                         </div>
-                        <div className="user-username">
-                            <form onSubmit={this.handleSubmit}>
+                        
 
-                                <label htmlFor="name">Firstname</label><br/>
-                                <div>
-                                    <div style={{ color:'red'}}></div>
-                                    <input type="text" name ="name" value={this.state.name.firstname} onChange={e => this.setState({ firstname: e.target.value })} placeholder="Firstname" /><br/>
-                                </div>
-                                
+                            <label htmlFor="surname">Surname</label><br/>
+                            <div>
+                            <div style={{ color:'red'}}></div>
+                            <input type="text" name="surname" value={this.state.surname} onChange={e => this.setState({surname: e.target.value })} placeholder="Surname"/>
+                            </div>
+                        
 
-                                 <label htmlFor="surname">Surname</label><br/>
-                                 <div>
-                                    <div style={{ color:'red'}}></div>
-                                    <input type="text" name="surname" value={this.state.name.surname} onChange={e => this.setState({surname: e.target.value })} placeholder="Surname"/>
-                                 </div>
-                                
-
-                                <hr/>
-                                <label htmlFor="phoneNumber" className="label-phone_number">mobile</label><br/>
-                                <div>
-                                    <div style={{ color:'red'}}></div>
-                                    <input type="text" name ="phone_number" value={this.state.phone_number} onChange={e => this.setState({ phone_number: e.target.value })} placeholder="+ 444 444 444" className="phone_number"/>
-                                </div>
-                                
-                                <button type="submit" className="btn-add_contact2" >
-                                    <i className="fas fa-plus"></i>
-                                    Update Number
-                                </button>
-                            </form>
+                        <hr/>
+                        <label htmlFor="phoneNumber" className="label-phone_number">Mobile</label><br/>
+                        <div>
+                            <div style={{ color:'red'}}></div>
+                            <input type="text" name ="phone_number" value={this.state.phone_number} onChange={e => this.setState({ phone_number: e.target.value })} placeholder="+ 444 444 444" className="phone_number"/>
                         </div>
-                    </div>
-                </div>   
+                        
+                        <button type="submit" className="btn-add_contact2" >
+                            <i className="fas fa-plus"></i>
+                            Update Number
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>   
             
         )
     }
